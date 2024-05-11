d99638d70dee458da414a7ad437c6f81d7e96524
aalopez76 <aalpzp@gmail.com>
2024-05-11 11:27:46 -0600

    add hola1.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ import streamlit as st
 st.title("Mi Primera App con Streamlit") 
 st.header("Información sobre el Conjunto de Datos")
 st.write("""Este es un simple ejemplo de una app""")
+
+st. header ("Cambio realizado")
